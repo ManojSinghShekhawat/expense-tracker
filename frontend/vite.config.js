e54f8256fd8fb0b45a20fd59600e27ee2a97771f
manoj singh <ma9j@hotmail.com>
2025-05-25 16:02:44 +0530

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://your-backend-url.onrender.com",
+        target: "https://expense-tracker-49lm.onrender.com",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
